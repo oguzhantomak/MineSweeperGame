12fb255a98c328fef21e14a052e0ed0eb55463f0
oguzhantomak <oguzhantomak@gmail.com>
2019-11-09 21:40:15 +0200

    Add project files.

---
+++ MineSweeperGame/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace MineSweeperGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flpMineArea = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flpMineArea
+            // 
+            this.flpMineArea.Location = new System.Drawing.Point(13, 13);
+            this.flpMineArea.Name = "flpMineArea";
+            this.flpMineArea.Size = new System.Drawing.Size(468, 468);
+            this.flpMineArea.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 493);
+            this.Controls.Add(this.flpMineArea);
+            this.Name = "Form1";
+            this.Text = "Mine Sweeper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flpMineArea;
+    }
+}
+
---
+++ MineSweeperGame/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeperGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            flpMineArea.Controls.Clear();
+            //PictureBox[] pbMines = new PictureBox[100];
+            for (int i = 0; i < 100; i++)
+            {
+                PictureBox minePb = new PictureBox();
+                minePb.Height = minePb.Width = 40;
+
+                minePb.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                int mine = rnd.Next(0, 2);
+                minePb.Text = mine.ToString();
+
+                //Kontroler üzerinde değer saklarken "TAG" değişkenini kullanırız. Tipi object olduğundan istediğimiz tipte değer saklamamızı sağlar.
+                minePb.Tag = mine;
+
+                //Kontrol Adı.Event Adı += TAB TAB
+                minePb.Click += MinePb_Click;
+
+
+                flpMineArea.Controls.Add(minePb);
+            }
+        }
+
+        private void MinePb_Click(object sender, EventArgs e)
+        {
+            //Tıklanan butonu yakalıyoruz.(Sender ediyoruz)
+            PictureBox minePb1 = (PictureBox)sender;
+            //MessageBox.Show(minePb1.Tag.ToString());
+
+            //Tıklanan butonun Tag bilgisini yani mayın olup olmadığını yani 0 veya 1 olduğunu mineInfo değişkenine atıyoruz.
+            int mineInfo = Convert.ToInt32(minePb1.Tag);
+
+            //Eğer mineInfo değişkeni yani tıklanan butonun tag'i 0 ise yani mayın değilse
+            if (mineInfo == 0)
+            {
+                //Tıklanan butonun arka plan rengini yeşil yap
+                minePb1.BackColor = Color.Green;
+            }
+            //Eğer tıklanan buton bir mayınsa
+            else
+            {
+                //flpMineArea isimli Flow Layout Panel'in içerisindeki controls(kontroller)'de dön, Control türünde item olarak tut.
+                foreach (Control item in flpMineArea.Controls)
+                {
+                    //PictureBox türünde minePb değişkeni oluştur ve control olarak dönen itemları PictureBox türünde tut.
+                    PictureBox minePb = item as PictureBox;
+
+                    //Eğer tuttuğumuz minePb değişkenindeki PictureBoxların taglari'i 1 ise
+                    if (Convert.ToInt32(minePb.Tag) == 1)
+                    {
+                        //Tag'i 1 olan bütün PictureBoxların arka planını siyah yap ve mayın simgesi getir.
+                        minePb.BackColor = Color.Black;
+                        minePb.Image = Image.FromFile("C:/Users/Ruben Castro/Desktop/Resimler/40x40.jpg");
+                    }
+                    else
+                    {
+                        //Tag'i 0 olan bütün PictureBoxlaron arka planını beyaz yap.
+                        minePb.BackColor = Color.White;
+                    }
+                }
+            }
+        }
+    }
+}
